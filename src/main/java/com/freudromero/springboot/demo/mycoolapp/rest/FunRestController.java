7f5174955761b4efb44f12a092d8ea7ac23f96ac
fred <abnertorres58@gmail.com>
2022-05-03 18:18:43 -0400

    Spring Boot -  adding Dev Tools

--- src/main/java/com/freudromero/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/freudromero/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -13,4 +13,18 @@ public class FunRestController {
     public String sayHello() {
         return "Hello world! Time on server is " + LocalDateTime.now();
     }
+
+    // Expose a new endpoint for "workout"
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+       return "Run a hard 5K!";
+    }
+
+    // Expose a new endpoint for "fortune"
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is your lucky day";
+    }
+
+
 }
